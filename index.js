525ba0c6e25361b676e8ce38e7172b8b11dddc14
wil004 <79720969+wil004@users.noreply.github.com>
2021-11-02 12:23:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -165,24 +165,6 @@ const inventory = [
 
 
 
-let allTvTypes = [];
-inventory.forEach(item => {
-    allTvTypes.push(item.type);
-});
-
-const allTvTypes1 = inventory.map(item => {
-    return item.type
-})
-
-
-let total = 0;
-
-inventory.forEach(item => {
-    total = item.originalStock - item.sold + total;
-
-});
-
-
 // Opdracht 1a: Gebruik een array-methode om een array te maken met alle tv-type namen.
 // Log de uitkomst in de console.
 
@@ -205,37 +187,144 @@ const tvSoldOut = inventory.filter((item) => {
     return item.options.ambiLight;});
 
 
-// Een functie om een uitklapbare button te maken voor html.
-
-    const ambiLightOption = (array) => {
-    let ambiLight1 = '';
-    for (let i = 0; i < array.length; i++) {
-        ambiLight1 = array[i].brand + ' ' + array[i].type + array[i].name + "<br>" + '€' + array[i].price + ",- <br>"
-            + "Television has ambiLight:" + ' ' + array[i].options.ambiLight + "<br><br><br>" + ambiLight1;
-    }
-    return ambiLight1
-}
 
 // Opdracht 1d: Schrijf een functie die alle tv's van laagste naar hoogste prijs sorteert.
 // Log de uitkomst in de console.
 
-const sorteer = inventory.sort(item = (a, b) => {
-    return a.price - b.price } );
+const sorteerTv = (array) => {
+    array.sort(item = (a, b) => {
+        return a.price - b.price
+    });
+    return array
+}
+   // console.log(sorteerTv(inventory))
 
-    console.log(sorteer)
 
-
-// console.log(ambiLightOption(ambiLight));
 
 // Opdracht 2a: Hoeveel tv's zijn er al verkocht?
 // Schrijf een script dat dit berekent. Log de uitkomst in de console.
 
+const totalTvSold = (array) => {
+        let sold = 0;
+        array.forEach(item =>
+        sold += item.sold);
+        return sold
+}
+// console.log(totalTvSold(inventory))
+
+
+
+// Opdracht 2b: Zorg ervoor dat dit aantal in het groen wordt weergegeven op de pagina.
+// Onderaan de pagina.
+
+// Opdracht 2c: Hoeveel tv's heeft Tech It Easy ingekocht?
+// Schrijf een script dat dit berekent. Log de uitkomst in de console.
+
+const boughtTv = (array) => {
+        let totalBought = 0;
+        array.forEach(item =>
+        totalBought += item.sold + item.originalStock)
+    return totalBought
+}
+
+// console.log(boughtTv(inventory))
+
+// Opdracht 2d: Zorg ervoor dat dit aantal in het blauw wordt weergegeven op de pagina.
+// Onderaan de pagina
+
+// Opdracht 2e: Geef in het rood weer hoeveel tv's er nog verkocht moeten worden.
+// Onderaan de pagina
+
+
+// Opdracht 3a: Gebruik een array-methode om alle tv merken (zoals Philips, NIKKEI, etc.) in een lijst op de pagina weer te geven.
+//    Zorg ervoor dat dit ook zou werken als we 200 tv's in onze array zouden hebben staan.
+//    Dat er dubbele namen in zitten, is niet erg.
+const tvBrands = (array) => {
+        let brandNames = ''
+        array.forEach(item => brandNames = brandNames + '| ' + item.brand )
+    return brandNames
+}
+// console.log(tvBrands(inventory))
+
+// Opdracht 3b: Schrijf de code uit 3a om naar een functie die een array met tv-objecten verwacht.
+//Het is handig om onze scripts als functies op te zetten, zodat we ze gemakkelijk kunnen hergebruiken.
+// Tip: vergeet deze functie -declaratie niet aan te roepen!
+
+// Had al een functie die tv-objecten verwacht aangemaakt bij 3A
+
+// Opdracht 4a: Zorg ervoor dat er een string wordt gegenereerd voor de naam van een tv.
+// Maak een functie die één enkel tv-object (zoals inventory[0] of inventory[6])
+// verwacht en de naam op de volgende manier samenvoegt: [merk] [type] - [naam] zoals P
+// hilips 43PUS6504/12 - 4K TV of NIKKEI NH3216SMART - HD smart TV.
+// Test of jouw functie ook werkt wanneer er een ander tv object wordt meegegeven.
+
+const tvName = (array) => {
+        return array.brand + ' ' + array.type + ' ' + array.name
+}
+// console.log(tvName(inventory[1]))
+
+// Opdracht 4b: Zorg ervoor dat de prijs van een tv netjes geformat wordt.
+// Maak een functie die één tv-prijs als parameter verwacht (zoals 379) en daar de volgende string van maakt: €379,-.
+// Test of jouw functie ook werkt wanneer er een andere tv-prijs wordt meegegeven.
+
+const priceFormat = (array) => {
+        return '€' + array.price + ',-'
+}
+
+// console.log(priceFormat(inventory[2]))
+
+// Opdracht 4c: Zorg ervoor dat er een string wordt gegenereerd voor alle beschikbare schermgroottes van één tv, in
+// zowel inches als cm.
+// Doe dit door een functie te schrijven die één screen-sizes array verwacht
+// ( zoals inventory[0].availableSizes) en de groottes op de volgende manier samenvoegt: [schermgrootte] inches
+// ([schermgrootte omgerekend]cm) | [schermgrootte] inches ([schermgrootte omgerekend]cm) etc.
+// Als een tv maar één schermgrootte heeft ([32]) wordt de output 32 inch (81 cm).
+// Wanneer een tv vier schermgroottes heeft
+// ([43, 50, 55, 58]) wordt de output 43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm).
+// Test of jouw functie werkt in alle mogelijke gevallen.
+
+const screenSizes = (array) => {
+    let screenSizes = ''
+    for(let i = 0; i < array.length; i++) {
+            screenSizes = screenSizes + array[i]  +  ' inch ' + '(' + Math.round(array[i] * 2.54) + ' cm)'
+        if (i < array.length - 1) {
+            screenSizes = screenSizes + ' | '
+        }
+        }
+       return screenSizes
+}
+
+// Opdracht 4d: Zorg ervoor de informatie van één van de tv's zoals het
+// voorbeeld wordt weergegeven op de pagina. Gebruik hiervoor de functies die je hebt gemaakt in opdracht 4a, 4b en 4c.
+const televisionStats = (array) => {
+    console.log(tvName(array));
+    console.log(priceFormat(array))
+    return screenSizes(array.availableSizes)
+}
+
+console.log(televisionStats(inventory[4]))
+
+const allTelevisionStats = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        console.log(tvName(array[i]));
+        console.log(priceFormat(array[i]))
+        console.log(screenSizes(array[i].availableSizes))
+    }
+    return 'The store currently has ' + array.length + ' different types of televisions!'
+}
+
+console.log(allTelevisionStats(inventory))
 
 
 
 
-//Opdracht 2b: Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die volledig uitverkocht zijn.
 
+
+
+// BONUS OPDRACHT De opdrachten 2b 2d en 2e zitten ook verwerkt in deze opdracht!
+
+
+// uitverkochte televisies!
 const soldOut = inventory.filter((item) => {
     return item.originalStock === item.sold
 })
@@ -249,20 +338,11 @@ const soldOutTelevisions = (array) => {
     return soldOutTv
 }
 
-
-//Opdracht 2c: Gebruik een array-methode om alle tv's te verzamelen (de hele objecten) die over AmbiLight beschikken.
-
-
-
-
-//Opdracht 2d: Schrijf een functie die alle tv's van laagste naar hoogste prijs sorteert.
+// Een functie om een uitklapbare button te maken.
 
 let sorteer1 = (a, b) => {
     return a.price - b.price;
 }
-
-// Een functie om een uitklapbare button te maken.
-
 const sorted = (array) => {
     inventory.sort(sorteer1);
     let sortedInventory = '';
@@ -273,81 +353,29 @@ const sorted = (array) => {
     return sortedInventory;
 };
 
+// Een functie om een uitklapbare button te maken voor html.
 
-//Opdracht 3a: Wat is onze doel-opbrengst? Bereken wat de totale opbrengst is, als we alle exemplaren van ieder
-// type zouden verkopen. Geef dit in het blauw weer op de pagina.
-
-function priceStock(inventory = []) {
-    let totalRevenue = 0;
-    for (let i = 0; i < inventory.length; i++) {
-        totalRevenue = (inventory[i].originalStock - inventory[i].sold) * inventory[i].price + totalRevenue;
+const ambiLightOption = (array) => {
+    let ambiLight1 = '';
+    for (let i = 0; i < array.length; i++) {
+        ambiLight1 = array[i].brand + ' ' + array[i].type + array[i].name + "<br>" + '€' + array[i].price + ",- <br>"
+            + "Television has ambiLight:" + ' ' + array[i].options.ambiLight + "<br><br><br>" + ambiLight1;
     }
-    return totalRevenue
+    return ambiLight1
 }
 
-let revenue = priceStock(inventory);
 
 
-//Opdracht 6b: Hoeveel hebben we tot nu toe verdient? Bereken hoeveel we tot nu toe verdient
-// hebben met het aantal verkochte tv's. Geef dit weer in het groen weer op de pagina
-
-let soldValue = 0;
-
-inventory.forEach(item => {
-    soldValue = (item.sold * item.price) + soldValue;
-});
-
-//Opdracht 4
-//Geef de type-namen van twee tv's weer op de pagina. Welke tv's dat precies zijn,
-// maakt niet zoveel uit. Voor nu betekent dit dat je het appenden van de nodes twee keer
-// moet uitschrijven, dat is niet erg!
-
-function TypeTwo(array) {
-    let type = array.type;
-    return type
-}
-
-const TypeTwo2 = TypeTwo(inventory[2]) + (TypeTwo(inventory[4]));
 
 
-//Opdracht 5a: Zorg ervoor dat er een string wordt gegenereerd voor de naam van een tv.
-// Maak een functie die één tv-object als parameter verwacht en de naam op de volgende
-// manier samenvoegt: [merk] [type] - [naam] zoals Philips 43PUS6504/12 - 4K TV of NIKKEI
-// NH3216SMART - HD smart TV. Zorg ervoor dat je deze functie voor iedere tv zou kunnen
-// gebruiken.
-
-function tvType(object) {
-    let brand = object.brand;
-    let type = object.type;
-    let name1 = object.name;
-    return brand + ': ' + type + ': ' + name1
-}
-
-const tvType1 = tvType(inventory[2]) + (tvType(inventory[4]));
 
 
-//Opdracht 5b: Zorg ervoor dat de prijs van een tv netjes geformat wordt.
-// Maak een functie die één tv-prijs als parameter verwacht (zoals 379) en
-// daar de volgende string van maakt: €379,-. Zorg ervoor dat je deze functie
-// voor iedere tv zou kunnen gebruiken.
-
-function tvPrice(priceNumber) {
-    let price = '€' + priceNumber + ",-";
-    return price
-}
-
-// console.log(tvPrice(inventory[2].price))
 
 
-//Opdracht 5c: Zorg ervoor dat er een string wordt gegenereerd voor alle beschikbare
-// schermgroottes van één tv in zowel inches als cm Maak een functie die één screen-sizes
-// array verwacht en de groottes op de volgende manier samenvoegt: [schermgrootte]
-// inches ([schermgrootte omgerekend]cm) | [schermgrootte] inches ([schermgrootte omgerekend]cm)
-// etc. Dus een input van [32] geeft 32 inch (81 cm) en een input van [43, 50, 55, 58]
-// geeft 43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm). Zorg ervoor dat je deze
-// functie voor iedere tv zou kunnen gebruiken, zowel voor tv's met maar één schermgrootte
-// als met tientallen schermgroottes.
-
+/* zelfgemaakte functie die eigenlijk hetzelfde doet als bij opdracht 4c.
+Het enige verschil is dat de index parameter hier optioneel is en je er ook voor kan kiezen
+de hele array availableSizes(inventory) in de parameter kan stoppen waardoor direct alle maten worden omgezet.
+ */
 
 let availableSizes = (inventory, index) => {
     let allSizes = []
@@ -369,6 +397,11 @@ let availableSizes = (inventory, index) => {
     }
 }
 
+
+/*Zoals ik hieronder in het engels beschreven heb ook bij deze functie is de index parameter optioneel
+je kunt ervoor kiezen 1 object aan te roepen printTv(array, 2)
+Of je kunt ervoor kiezen een hele array met bijv 200 objecten aan te roepen printTv(inventory).
+ */
 
 /* The array parameter most be declared with the 'inventory' array of existing out of objects.
  the index parameter is optional! In this parameter you declare the index of the array to get a specified product!
@@ -394,3 +427,125 @@ const printTv = (array, index) => {
 
     }
 }
+
+//bonusOpdrachten
+function sortFunction() {
+    document.getElementById("sort").innerHTML = sorted(inventory);
+}
+
+function ambiLightFunction() {
+    document.getElementById("ambilight").innerHTML = ambiLightOption(ambiLight);
+}
+
+function soldOutFunction() {
+    document.getElementById("soldOut").innerHTML = soldOutTelevisions(soldOut);
+}
+//einde bonusopdrachten
+
+//2b c en e
+
+const tvSold = totalTvSold(inventory)
+
+const tvSoldHtml = document.getElementById('tvSold');
+tvSoldHtml.innerHTML = tvSold;
+
+const tvBuyIn = boughtTv(inventory)
+
+const boughtTvsHtml = document.getElementById('tvBuyIn');
+boughtTvsHtml.innerHTML = tvBuyIn;
+
+const tvStock = (array) => {
+    let totalStock = 0;
+    array.forEach(item =>
+    totalStock += (item.originalStock - item.sold))
+    return totalStock;
+}
+
+const totalTvStock = document.getElementById('tvStock');
+totalTvStock.innerHTML = tvStock(inventory)
+
+
+const printTelevisions = document.getElementById('printTelevisions');
+printTelevisions.innerHTML = printTv(inventory);
+// einde opdrachten 2b c en e
+
+
+//Hieronder zie je zelfstudie voor prestatieverbetering!
+
+// 0
+
+const printTvHtml0 = document.getElementById("tvType0");
+printTvHtml0.innerHTML = printTv(inventory, 0)
+
+const chooseSize0 = availableSizes(inventory, 0);
+const chooseSizeHtml0 = document.getElementById('tvSizeSelect0')
+for(let i = 0; i < chooseSize0.length; i++){
+    chooseSizeHtml0.options.add(new Option(chooseSize0[i])); }
+
+
+//1
+
+const printTvHtml1 = document.getElementById("tvType1");
+printTvHtml1.innerHTML = printTv(inventory, 1)
+
+const chooseSize1 = availableSizes(inventory, 1);
+const chooseSizeHtml1 = document.getElementById('tvSizeSelect1')
+for(let i = 0; i < chooseSize1.length; i++){
+    chooseSizeHtml1.options.add(new Option(chooseSize1[i])); }
+
+
+// 2
+
+const printTvHtml2 = document.getElementById("tvType2");
+printTvHtml2.innerHTML = printTv(inventory, 2)
+
+const chooseSize2 = availableSizes(inventory, 2);
+const chooseSizeHtml2 = document.getElementById('tvSizeSelect2')
+for(let i = 0; i < chooseSize2.length; i++){
+    chooseSizeHtml2.options.add(new Option(chooseSize2[i])); }
+
+// 3
+
+const printTvHtml3 = document.getElementById("tvType3");
+printTvHtml3.innerHTML = printTv(inventory, 3)
+
+const chooseSize3 = availableSizes(inventory, 3);
+const chooseSizeHtml3 = document.getElementById('tvSizeSelect3')
+for(let i = 0; i < chooseSize3.length; i++){
+    chooseSizeHtml3.options.add(new Option(chooseSize3[i])); }
+
+//4
+const printTvHtml4 = document.getElementById("tvType4");
+printTvHtml4.innerHTML = printTv(inventory, 4)
+
+const chooseSize4 = availableSizes(inventory, 4);
+const chooseSizeHtml4 = document.getElementById('tvSizeSelect4')
+for(let i = 0; i < chooseSize4.length; i++){
+    chooseSizeHtml4.options.add(new Option(chooseSize4[i])); }
+
+//5
+const printTvHtml5 = document.getElementById("tvType5");
+printTvHtml5.innerHTML = printTv(inventory, 5)
+
+const chooseSize5 = availableSizes(inventory, 5);
+const chooseSizeHtml5 = document.getElementById('tvSizeSelect5')
+for(let i = 0; i < chooseSize5.length; i++){
+    chooseSizeHtml5.options.add(new Option(chooseSize5[i])); }
+
+//6
+const printTvHtml6 = document.getElementById("tvType6");
+printTvHtml6.innerHTML = printTv(inventory, 6)
+
+const chooseSize6 = availableSizes(inventory, 6);
+const chooseSizeHtml6 = document.getElementById('tvSizeSelect6')
+for(let i = 0; i < chooseSize6.length; i++){
+    chooseSizeHtml6.options.add(new Option(chooseSize6[i])); }
+
+//6
+const printTvHtml7 = document.getElementById("tvType7");
+printTvHtml7.innerHTML = printTv(inventory, 7)
+
+const chooseSize7 = availableSizes(inventory, 7);
+const chooseSizeHtml7 = document.getElementById('tvSizeSelect7')
+for(let i = 0; i < chooseSize7.length; i++){
+    chooseSizeHtml7.options.add(new Option(chooseSize7[i])); }
